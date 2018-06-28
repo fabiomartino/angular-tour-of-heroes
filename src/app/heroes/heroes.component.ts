63abed64ae2ecad075aa25632f99dc7832bf3f3f
Fabio Martino <martinofabio@libero.it>
2018-06-28 22:34:37 +0200

    Complete step: 'Display a Heroes List'

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './heroes.model';
+import { HEROES } from '../../assets/mocks/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +9,15 @@ import { Hero } from './heroes.model';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() { }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
